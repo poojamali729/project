cedfeb17489dda4d604671f6eaea9112f431be8f
poojamali729 <pm774190@gmail.com>
2024-02-12 21:51:35 -1200

    traveling web has been complete

--- traveling-website/js/script.js
+++ traveling-website/js/script.js
@@ -3,3 +3,16 @@ const header = document.querySelector("header");
 window.addEventListener("scroll",function() {
     header.classList.toggle("sticky", window.scrollY > 60)
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('ba-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('ba-x');
+    navlist.classList.remove('open');
+}
